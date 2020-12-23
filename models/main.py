54d0971e0ed7d8c08ae6af1fb57ef4659fc45431
Sasha <q.dev.null@gmail.com>
2020-12-23 07:57:08 +0200

    Add the the transformations to the main pipeline

--- models/main.py
+++ models/main.py
@@ -3,13 +3,14 @@ from click import Path as cpath
 
 from pathlib import Path
 from models.dataset import RawDataset
+from models.augmentations import transform
 
 
 @click.command()
 @click.option("--fin", type=cpath(exists=True))
 def main(fin):
     train_folders = list(Path(fin).glob("**/*/*/"))
-    train = RawDataset(train_folders)
+    train = RawDataset(train_folders, transform=transform())
 
     tile, mask = train[0]
     print(tile.shape, mask.shape)
