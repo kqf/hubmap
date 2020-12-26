4235311a54d57f76507ebe13035abc0c969c750f
kqf <q.dev.null@gmail.com>
2020-12-26 11:44:19 +0200

    Add compare plotting method (#21)
    * Add compare plotting method
    
    * Simplify the plotting methods
    
    * Check the plotting for the pipeline without augmentations

--- models/main.py
+++ models/main.py
@@ -4,18 +4,20 @@ from click import Path as cpath
 from pathlib import Path
 from models.dataset import RawDataset
 from models.augmentations import transform
-from models.plot import glance
+from models.plot import glance, compare
 
 
 @click.command()
 @click.option("--fin", type=cpath(exists=True))
 def main(fin):
-    train_folders = list(Path(fin).glob("*/*"))
-    train = RawDataset(train_folders, transform=transform())
+    train_folders = list(Path(fin).glob("*/*"))[:20]
+    train = RawDataset(train_folders, transform=transform(train=False))
 
     tile, mask = train[0]
     print(tile.shape, mask.shape)
-    glance(train, batch_size=5)
+
+    for image, mask in train:
+        compare(image, mask)
 
 
 if __name__ == '__main__':
--- models/plot.py
+++ models/plot.py
@@ -1,14 +1,16 @@
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.transforms.functional import to_pil_image
 
 
-def tensor2img(t, padding=16):
+def tensor2img(t, padding=0):
     std = torch.Tensor([0.229, 0.224, 0.225]).reshape(-1, 1, 1)
     mu = torch.Tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1)
+    # return t * std + mu if t.shape[0] > 1 else t
     img = to_pil_image(t * std + mu if t.shape[0] > 1 else t)
     w, h = img.size
-    return img.crop((padding, padding, w - padding, h - padding))
+    return np.array(img.crop((padding, padding, w - padding, h - padding)))
 
 
 def plot(*imgs):
@@ -32,12 +34,23 @@ def plot(*imgs):
     return axes
 
 
-def glance(dataset, batch_size):
+def batches(dataset, batch_size):
     batch = []
     for pair in dataset:
         if len(batch) < batch_size:
+            compare([pair[0]], [pair[1]])
             batch.append(pair)
             continue
-
-        plot(*zip(*batch))
+        yield list(zip(*batch))
         batch = []
+
+
+def compare(image, mask):
+    plt.imshow(tensor2img(image))
+    plt.imshow(tensor2img(mask), alpha=0.6)
+    plt.show()
+
+
+def glance(dataset, batch_size, pfunc=plot):
+    for batch in batches(dataset, batch_size):
+        pfunc(*batch)
