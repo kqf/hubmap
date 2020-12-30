6217e9565f6f15c2e4147a9d045fee8aeb896925
Sasha <q.dev.null@gmail.com>
2020-12-30 07:35:12 +0200

    Fix the normalization

--- models/augmentations.py
+++ models/augmentations.py
@@ -1,14 +1,14 @@
 import albumentations as alb
 from albumentations.pytorch import ToTensorV2
 
+_mean = [0.485, 0.456, 0.406],
+_std = [0.229, 0.224, 0.225]
 
-def transform(train=True):
+
+def transform(train=True, mean=None, std=None):
     normalize = alb.Compose([
         alb.PadIfNeeded(256, 256),
-        alb.Normalize(  # TODO: Fix me
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        ),
+        alb.Normalize(mean=mean or _mean, std=std or _std),
         ToTensorV2(),
     ])
 
