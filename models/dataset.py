94b78c9b03c2eb55c7852b74bb086052cce89e6f
Sasha <q.dev.null@gmail.com>
2020-12-30 07:30:53 +0200

    Remove the unnecessary type conversion

--- models/dataset.py
+++ models/dataset.py
@@ -21,12 +21,11 @@ class RawDataset(Dataset):
 
     def __getitem__(self, idx):
         sample = self.samples[idx]
-        image = cv2.imread(str(sample / "tile.png"))
-        mask = cv2.imread(str(sample / "mask.png"), cv2.IMREAD_GRAYSCALE)
 
         # By default OpenCV uses BGR color space for color images,
         # so we need to convert the image to RGB color space.
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.imread(str(sample / "tile.png"), cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(str(sample / "mask.png"), cv2.IMREAD_GRAYSCALE)
 
         augmented = self.transform(image=image, mask=mask)
         image = augmented['image']
