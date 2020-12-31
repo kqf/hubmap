fbe02518938010f9cc1a4e1adb833ad27af2bce7
Sasha <q.dev.null@gmail.com>
2020-12-31 11:04:31 +0200

    Fix the callbacks

--- models/callbacks.py
+++ models/callbacks.py
@@ -4,8 +4,8 @@ import torchvision
 
 class TensorBoardWithImages(skorch.callbacks.TensorBoard):
     def on_batch_end(self, net, X, y, training, loss, y_pred):
-        label_grid = torchvision.utils.make_grid(y)
+        label_grid = torchvision.utils.make_grid(y.unsqueeze(1))
         self.writer.add_image("true_labels", label_grid)
 
-        pred_grid = torchvision.utils.make_grid(y_pred)
+        pred_grid = torchvision.utils.make_grid(y_pred.unsqueeze(1))
         self.writer.add_image("true_labels", pred_grid)
