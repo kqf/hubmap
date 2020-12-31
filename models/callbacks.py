caf563b0714f2b8ca914c5f505c36c85c3351253
Sasha <q.dev.null@gmail.com>
2020-12-31 12:36:48 +0200

    Log every 10th image

--- models/callbacks.py
+++ models/callbacks.py
@@ -4,8 +4,13 @@ import torchvision
 
 class TensorBoardWithImages(skorch.callbacks.TensorBoard):
     def on_batch_end(self, net, X, y, training, loss, y_pred):
-        label_grid = torchvision.utils.make_grid(y.unsqueeze(1))
-        self.writer.add_image("true_labels", label_grid)
+        n_batches = len(net.history[-1, "batches"])
+
+        if (n_batches % 10) != 0:
+            return
+
+        label_grid = torchvision.utils.make_grid(y.unsqueeze(1).float())
+        self.writer.add_image("true_labels", label_grid, n_batches)
 
         pred_grid = torchvision.utils.make_grid(y_pred.unsqueeze(1))
-        self.writer.add_image("pred_labels", pred_grid)
+        self.writer.add_image("pred_labels", pred_grid, n_batches)
