5ea3390d5c71f022513e826a41aa0816daf5b7da
kqf <q.dev.null@gmail.com>
2020-12-31 08:10:18 +0200

    Setup training (#33)
    * Ignore the training files
    
    * Add the training endpoint
    
    * Dump the pickled files

--- models/main.py
+++ models/main.py
@@ -4,20 +4,18 @@ from click import Path as cpath
 from pathlib import Path
 from models.dataset import RawDataset
 from models.augmentations import transform
-from models.plot import compare
+from models.model import build_model
 
 
 @click.command()
 @click.option("--fin", type=cpath(exists=True))
 def main(fin):
-    train_folders = list(Path(fin).glob("*/*"))[:20]
+    train_folders = list(Path(fin).glob("*/*"))
     train = RawDataset(train_folders, transform=transform(train=False))
 
-    tile, mask = train[0]
-    print(tile.shape, mask.shape)
-
-    for image, mask in train:
-        compare(image, mask)
+    model = build_model()
+    model.fit(train)
+    model.save_params(f_params='final.pkl')
 
 
 if __name__ == '__main__':
