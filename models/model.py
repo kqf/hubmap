9ff10befe4b16ab81231dd57a9ccdbd8afb87b9d
kqf <q.dev.null@gmail.com>
2021-01-02 09:28:20 +0200

    Fix deprecation warnings (#38)
    * Switch to sigmoid instead of softmax
    
    * Step every batch

--- models/model.py
+++ models/model.py
@@ -97,7 +97,8 @@ def build_model(max_epochs=2):
         base_lr=0.002,
         max_lr=0.2,
         step_size_up=2500,
-        step_size_down=2500
+        step_size_down=2500,
+        step_every='batch',
     )
 
     model = SegmentationNet(
