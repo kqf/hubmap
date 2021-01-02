4b1dceb461056e9dc4ffea06916a1ddd03a3349a
kqf <q.dev.null@gmail.com>
2021-01-02 11:18:57 +0200

    Add the dice coefficient (#39)
    * Add the dice coefficient implementation
    
    * Add the tests to check the metrics
    
    * Squeeze the threshold dimension for scalars

---
+++ tests/test_metrics.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+
+from models.metrics import dice
+
+
+@pytest.fixture
+def masks(batch_size, h, w):
+    return np.random.randint(0, 2, (batch_size, h, w))
+
+
+@pytest.fixture
+def probas(batch_size, h, w):
+    return np.random.uniform(0, 1, (batch_size, h, w))
+
+
+@pytest.mark.parametrize("batch_size, h, w", [
+    (32, 256, 256),
+])
+@pytest.mark.parametrize("thresholds", [
+    0.5,
+    np.arange(0.1, 0.9, 0.01)
+])
+def test_dice(batch_size, masks, probas, thresholds):
+    expected_shape = (batch_size,) + np.shape(thresholds)
+    assert dice(masks, probas, thresholds).shape == expected_shape
