22fb7d0d72c23a4f82eb16cc849b8e5f2e606520
Sasha <q.dev.null@gmail.com>
2021-01-02 10:25:42 +0200

    Add the dice coefficient implementation

--- models/metrics.py
+++ models/metrics.py
@@ -10,3 +10,24 @@ def iou_approx(true_masks, probas, padding=0):
     approx_intersect = np.sum(np.minimum(preds, true_masks), axis=(1, 2))
     approx_union = np.sum(np.maximum(preds, true_masks), axis=(1, 2))
     return np.mean(approx_intersect / approx_union)
+
+
+def dice(probas, masks, th):
+    # Ensure threshold dimension: [b, h, w] -> [b, h, w, 1]
+    probas = probas[..., None]
+    masks = masks[..., None]
+
+    # [b, h, w, t]
+    preds = probas > th
+
+    # [b, h, w, t]
+    inter = (preds * masks).sum(axis=(1, 2))
+
+    # [b, h, w, t]
+    union = (preds + masks).sum(axis=(1, 2))
+
+    # Handle the situation when union is zero
+    regularized_inter = np.where(union > 0.0, inter, np.zeros_like(union))
+    regularized_union = np.where(union > 0.0, union, np.ones_lik(union))
+
+    return 2 * regularized_inter / regularized_union
