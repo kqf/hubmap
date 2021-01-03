cb661081df43e28893d30097edbd93ec92ec59bc
kqf <q.dev.null@gmail.com>
2021-01-03 08:06:36 +0200

    Fix tensorboard logging (#40)
    * Add message support
    
    * Fix the messaging mechanism
    
    * Fix the global step calculation
    
    * Fix the missing quotes

--- models/callbacks.py
+++ models/callbacks.py
@@ -5,13 +5,14 @@ import torchvision
 
 class TensorBoardWithImages(skorch.callbacks.TensorBoard):
     def on_batch_end(self, net, X, y, training, loss, y_pred):
-        n_batches = len(net.history[-1, "batches"])
-        if (n_batches % 10) != 0:
+        global_step = sum(len(epoch["batches"]) for epoch in net.history)
+
+        if (global_step % 10) != 0:
             return
 
         label_grid = torchvision.utils.make_grid(y.unsqueeze(1).float())
-        self.writer.add_image("true_labels", label_grid, n_batches)
+        self.writer.add_image("true_labels", label_grid, global_step)
 
         probas = torch.sigmoid(y_pred)
         pred_grid = torchvision.utils.make_grid(probas.unsqueeze(1))
-        self.writer.add_image("pred_labels", pred_grid, n_batches)
+        self.writer.add_image("pred_labels", pred_grid, global_step)
