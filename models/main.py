7ebb3a1fb2b90b0f45fa993de56b1d0923ed8f22
kqf <q.dev.null@gmail.com>
2021-01-03 10:23:46 +0200

    Use test set for validation (#41)
    * Add phony rules
    
    * Make deterministic training
    
    * Fix the preprocessing step
    
    * Update the mean and std values for the dataset
    
    * Fix the main target
    
    * Fix the splits
    
    * Fix the tests

--- models/main.py
+++ models/main.py
@@ -1,12 +1,28 @@
 import os
+
 import click
-from click import Path as cpath
+import torch
+import random
+import numpy as np
 
 from pathlib import Path
+from click import Path as cpath
+from environs import Env
+from sklearn.model_selection import train_test_split
+from skorch.helper import predefined_split
+
 from models.dataset import RawDataset
 from models.augmentations import transform
 from models.model import build_model
-from environs import Env
+
+
+SEED = 137
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
 
 
 @click.command()
@@ -16,8 +32,10 @@ def main(fin, message):
     if not message:
         raise IOError("Please provide the train message")
 
-    train_folders = list(Path(fin).glob("*/*"))
-    train = RawDataset(train_folders, transform=transform(train=False))
+    folders = list(Path(fin).glob("*/*"))
+    ftrain, ftest = train_test_split(folders, random_state=SEED)
+    train = RawDataset(ftrain, transform=transform(train=False))
+    test = RawDataset(ftest, transform=transform(train=False))
 
     env = Env()
     env.read_env()
@@ -33,7 +51,8 @@ def main(fin, message):
     model = build_model(
         max_epochs=50,
         logdir=logdir,
-        logdir_local=logdir_local
+        logdir_local=logdir_local,
+        train_split=predefined_split(test),
     )
     model.fit(train)
     model.save_params(f_params=os.path.join(logdir_local, 'final.pkl'))
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,11 @@
 from models.dataset import RawDataset
 from models.augmentations import transform
 from models.model import build_model
+from skorch.helper import predefined_split
 
 
 def test_model(fake_dataset):
     dataset = RawDataset(list(fake_dataset.glob("*/")), transform=transform())
 
-    model = build_model()
+    model = build_model(train_split=predefined_split(dataset))
     model.fit(dataset)
