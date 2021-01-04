f15b180b46070fe27a17f5a74ce40e86cc7f7b98
Sasha <q.dev.null@gmail.com>
2021-01-04 08:16:29 +0200

    Add blob 2 image convertor

--- models/mc.py
+++ models/mc.py
@@ -15,3 +15,15 @@ def make_blob(h=256, w=256, lam=5, area=8, std_area=1):
 
     mask = dists <= radii
     return mask.sum(axis=-1).astype(bool)
+
+
+def blob2image(blob, channels=3, epsilon=0.1):
+    h, w = blob.shape
+
+    extended = blob[..., None]
+
+    # Add a small term to add noise to the empty regions
+    noise = np.random.poisson(extended + epsilon, size=(h, w, channels))
+
+    # Convet to image scale
+    return extended + noise * 255
--- tests/test_mc.py
+++ tests/test_mc.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
-from models.mc import make_blob
+from models.mc import make_blob, blob2image
 
 
 def test_generates():
-    plt.imshow(make_blob())
+    blob = make_blob()
+    plt.imshow(blob)
+    plt.show()
+
+    img = blob2image(blob)
+    plt.imshow(img)
     plt.show()
