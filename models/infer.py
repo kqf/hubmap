22babc0a06f22c2fecae2838401bcdaae04fe98c
kqf <q.dev.null@gmail.com>
2021-01-14 20:26:59 +0100

    Increase the crop size at inference (#69)

--- models/infer.py
+++ models/infer.py
@@ -149,9 +149,15 @@ class InferenceModel:
                     yield torch.sigmoid(flip(logits)).detach()
 
 
-def predict_masks(df, trainpath, models=[],
-                  sz=256, reduction=4,
-                  pthreshold=0.10, batch_size=32):
+def predict_masks(
+    df,
+    trainpath,
+    models=[],
+    sz=512,
+    reduction=2,
+    pthreshold=0.20,
+    batch_size=32
+):
     preds, names = [], []
     for idx, (sample, *_) in tqdm(df.iterrows(), total=len(df)):
         tiff = (trainpath / sample).with_suffix(".tiff")
