663d5edc953fcb87ea1ad5bdba632b6c73371976
kqf <q.dev.null@gmail.com>
2021-01-19 13:44:13 +0100

    Finalize the resUNet (#75)
    * Use realistic image-sizes; Fix the imports
    
    * Fix the channels mismatch for the resnet; Fix the degrading scale
    
    * Remove the breakpoints
    
    * Fix a typo

--- models/modules.py
+++ models/modules.py
@@ -56,17 +56,6 @@ class UNet(torch.nn.Module):
         return self.final(dec1)
 
 
-def get_encoder(self, encoder, layer):
-    if layer == 0:
-        return
-    elif layer == 2:
-        return encoder.layer2
-    elif layer == 3:
-        return encoder.layer3
-    elif layer == 4:
-        return encoder.layer4
-
-
 class ResUNet(torch.nn.Module):
     def __init__(self, pretrained=False):
         super().__init__()
@@ -85,20 +74,17 @@ class ResUNet(torch.nn.Module):
         self.conv5 = encoder.layer4
 
         self.center = torch.nn.Sequential(
-            torch.nn.MaxPool2d(2),  # MaxPool
-            make_decoder_block(512, 512, 256)
+            torch.nn.MaxPool2d(2),
+            make_decoder_block(512, 512, 512)
         )
 
-        self.center = torch.nn.Identity()
+        # self.center = torch.nn.Identity()
 
-        self.dec5 = make_decoder_block(256 + 512, 512, 256)
-        self.dec4 = make_decoder_block(256 + 512, 512, 256)
-        self.dec3 = make_decoder_block(256 + 256, 256, 64)
-        self.dec2 = make_decoder_block(64 + 128, 128, 32)
-        self.dec1 = torch.nn.Sequential(
-            torch.nn.Conv2d(32 + 64, 32, 3, padding=1),
-            torch.nn.ReLU(inplace=True)
-        )
+        self.dec5 = make_decoder_block(512 + 512, 512, 256)
+        self.dec4 = make_decoder_block(256 + 256, 256, 128)
+        self.dec3 = make_decoder_block(128 + 128, 128, 64)
+        self.dec2 = make_decoder_block(64 + 64, 64, 64)
+        self.dec1 = make_decoder_block(64 + 64, 64, 32)
         self.final = torch.nn.Conv2d(32, 1, kernel_size=1)
 
     def forward(self, x):
@@ -111,10 +97,8 @@ class ResUNet(torch.nn.Module):
         center = self.center(conv5)
 
         dec5 = self.dec5(torch.cat([center, conv5], 1))
-
         dec4 = self.dec4(torch.cat([dec5, conv4], 1))
         dec3 = self.dec3(torch.cat([dec4, conv3], 1))
         dec2 = self.dec2(torch.cat([dec3, conv2], 1))
         dec1 = self.dec1(torch.cat([dec2, conv1], 1))
-
         return self.final(dec1)
